1ec8ebc60e5b051ba023b3a3d752ad07a997197c
HJfod <60038575+HJfod@users.noreply.github.com>
2022-02-16 22:02:48 +0200

    c#

---
+++ src/Style.cs
@@ -0,0 +1,9 @@
+using System.Windows.Media;
+
+namespace GeodeApp {
+    static class Theme {
+        public static class Colors {
+            public static SolidColorBrush Background = new SolidColorBrush(Color.FromRgb(30, 30, 30));
+        }
+    }
+}
---
+++ src/windows/MainWindow.cs
@@ -0,0 +1,8 @@
+
+namespace GeodeApp {
+    class MainWindow : AppWindow {
+        public MainWindow() : base("Geode App", 600, 400) {
+            
+        }
+    }
+}
